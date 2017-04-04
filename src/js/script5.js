33c978eb2cfca50e637a654aa1a70b31d4b792c1
Sofia Ödegaard Jacobsson <Sofia degaard Jacobsson>
2017-04-04 11:42:41 +0200

    fixed x axis months and removed click event on months

--- src/js/script5.js
+++ src/js/script5.js
@@ -3,12 +3,7 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40};
 var width = +svg.attr("width") - margin.left - margin.right;
 var height = +svg.attr("height") - margin.top - margin.bottom;
 
-/* Variable that holds the mean temperatures for each year */
-var dataYear = d3.nest()
-    .key(function(d) { return +d.year;})
-    .rollup(function(d) { 
-        return d3.mean(d, function(g) {return g.adj; });
-    }).entries(json_data);
+var monthsArray = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 /* Variable that holds the data used to visualize the chart */
 var data = d3.nest()
@@ -85,6 +80,12 @@ g.selectAll(".bar")
 
 /* Updates the chart to show the mean temperature each year */
 function sortByYear() {
+    var dataYear = d3.nest()
+    .key(function(d) { return +d.year;})
+    .rollup(function(d) { 
+        return d3.mean(d, function(g) {return g.adj; });
+    }).entries(json_data);
+
     g.selectAll(".bar").data(dataYear);
     x.domain([1756, 2015]);
     y.domain([0, d3.max(dataYear, function(d) { return d.value; })]);
@@ -198,21 +199,26 @@ function sortByChosenYear(year) {
 
         console.log(groupedData);
 
-    g.selectAll(".bar").data(groupedData);
+    g.selectAll(".bar").data(groupedData)
+    .on("click", null);;
     
     // Scale the range of the data again
     //x = d3.scaleTime();
 
-    x.domain([0.5, 12.9]);
+    //x.domain([new Date(2017,0,1), new Date(2017, 11, 31)]);
+    x.domain([0.5, 12.9])
+    console.log(x.domain());
     y.domain([Math.min(d3.min(groupedData, function(d) { return d.value; }), 0), d3.max(groupedData, function(d) { return d.value; })]);
     /* Update the axes */
     xAxis = d3.axisBottom(x)
                     .tickSize(5)
+                    .ticks(12)
+                    .tickFormat(function(d){ {return monthsArray[d-1];}})
                     //.tickFormat(function(d){if (d % 10 == 0) {return d;}})
                     .tickPadding(3)
                     //.ticks(26);
                     //.ticks(d3.time.months)
-                    .tickFormat(d3.timeFormat("%b"));
+                    //.tickFormat(d3.timeFormat("%B"));
     yAxis = d3.axisLeft(y)
                     .tickSize(5)
                     .tickPadding(3)
